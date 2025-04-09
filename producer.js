2b9186213acf2977e18c8c5a07ea1e7923bbd91a
vadym <vadymcdevelweb@gmail.com>
2025-04-09 17:34:20 +0300

    producer dev

--- producer.js
+++ producer.js
@@ -1,2 +1,11 @@
 const Redis = require("ioredis")
 const { redisStreamKey, numberRange } = require("./config")
+
+async function startProducing() {
+	while (true) {
+		const num = Math.floor(Math.random() * (max + 1))
+		await redis.xadd(redisStreamKey, "*", "number", num.toString())
+	}
+}
+
+startProducing()
